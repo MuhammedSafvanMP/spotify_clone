4a2dd956a1721ff2679c7655492d79b10865f007
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2024-05-29 20:37:13 +0530

    created song model

---
+++ Backend/models/songModel.js
@@ -0,0 +1,27 @@
+const { default: mongoose } = require("mongoose");
+const mongoDB = require("mongoose");
+
+// creating user mongoose schema
+
+const songSchema  = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    thumbnail: {
+        type: String,
+        required: true
+    },
+    track: {
+        type: String,
+        required: true
+    },
+    artist: {
+        type: mongoose.Types.ObjectId,
+        ref: "user"
+    }
+  })
+
+const songModel = new mongoose.model("Song", songSchema);
+module.exports = songModel;
+
