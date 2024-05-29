382894d510a49a9a1c20f467adb37a39a855e5c4
MUHAMMED SAFVAN MP <muhammedsafvanmp7@gmail.com>
2024-05-29 20:00:10 +0530

    DB connected

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 9000
 
+// mongoDB atlas connecting 
 
+mongoose.connect(process.env.DB)
+.then(() => console.log("DB connected"))
+.catch((error) => console.log(error));
+
+
+// server creating
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
